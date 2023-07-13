9f4bcf9317b4cd670d662a58418a34c7a7e0aa58
surajsj17 <120768713+surajsj17@users.noreply.github.com>
2023-07-14 00:37:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,30 +20,4 @@ variable "instance_count" {
   description = "Number of instances to create"
   default     = 1  # Change this value as needed
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = " "
-    region         = "ap-south-1"
-    dynamodb_table = "your-dynamodb-lock-table"
-  }
-}
-
-
-resource "aws_s3_bucket" "example" {
-  bucket = "your-terraform-state-bucket"
-  acl    = "private"
-#   # ... add more configuration as needed
- }
-
-resource "aws_dynamodb_table" "lock_table" {
-  name           = "your-dynamodb-lock-table"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  # ... add more configuration as needed
 }
