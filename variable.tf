c66bdff274f700285bf0c5153889e5b06311d713
surajsj17 <120768713+surajsj17@users.noreply.github.com>
2023-07-14 00:35:03 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "my-access-key"{
-    default = "AKIA2JHC6WYWHNJEGWVB"
+    default = "AKIA2JHC6WYWMUWINJPK"
 }
 variable "my-secret-key" {
-    default = "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"
+    default = "PJ/RMnCw2md2r1Ih+a+QA1JZoJgNQLygbBkhnjt5"
 }
 variable "my-ami" {
     default = "ami-0d13e3e640877b0b9"
